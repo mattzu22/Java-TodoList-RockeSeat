dcdb5f0aed68796036f440fb7d17e08e5d02f9bf
Matheus Macedo <matheusg3.201168@gmail.com>
2023-10-10 18:59:42 -0300

    adding user data to an in-memory database - class 2

--- src/main/java/br/com/matheusmacedo/todolist/user/UserController.java
+++ src/main/java/br/com/matheusmacedo/todolist/user/UserController.java
@@ -1,5 +1,10 @@
 package br.com.matheusmacedo.todolist.user;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,22 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
  * Private
  * Protect
 */
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    /* 
-     * String (texto)
-     * Integer (int) numeros inteiros
-     * Double (double) numeros quebrados 0.000
-     * Float (float) numeros quebrados só que menores 0.00
-     * char (A C ) são caracteres
-     * Date (data)
-     * Void 
-    */
-    // annotation @
+
+    @Autowired
+    private IUserRepository userRepository;
+  
     @PostMapping("/")
-    public void create(@RequestBody UserModel UserModel){
-        System.out.println(UserModel.userName);
+    public ResponseEntity create(@RequestBody UserModel UserModel){
+        var user = this.userRepository.findByUserName(UserModel.getUserName());
+
+        if(user != null) {
+          return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Úsuario já esxiste");
+        }
+
+        var userCreated = this.userRepository.save(UserModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
-    
 }
